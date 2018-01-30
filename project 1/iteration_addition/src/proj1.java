df5e107ab64b7284941f5ccef7b05e72dfb0cea6
Joey <sharpshooter30@gmail.com>
2018-01-29 17:26:26 -0700

    working alorithm

--- project 1/iteration_addition/src/proj1.java
+++ project 1/iteration_addition/src/proj1.java
@@ -15,7 +15,7 @@ public class proj1 {
 
 		int n = 0;
 
-		ArrayList<Integer> answer = new ArrayList<Integer>();
+		String answer = "";
 		results = new ArrayList<ArrayList<Integer>>();
 		
 		//No command input ask for input
@@ -53,55 +53,19 @@ public class proj1 {
 
 		partition(n, n, answer);
 
-		printer();
-
 	}
 	
-    @SuppressWarnings("unchecked")
-	public static void partition(int n, int start, ArrayList<Integer> answer) {
-        if (n == 0) {
-            //System.out.println("hi " + answer);
-           
-            results.add((ArrayList<Integer>)answer.clone());
-            //System.out.println("Bye: " + results);
-            answer.clear();
+	public static void partition(int n, int start, String answer) {
+		if (n == 0) {           
+			System.out.println(answer);
+
             return;
         }
   
-        for (int i = Math.min(n, start); i > 0; i--) {
-        	answer.add(i);
+        for (int i = Math.min(start, n); i >= 1; i--) {
         	
-        	int sum = 0;
-        	
-        	for (int j = 0; j < answer.size(); j++) {
-        		if (!(sum == start) || (sum + answer.get(j) <= start)) {
-        			sum += answer.get(j);
-        		}
-        		
-        	}
-        	
-            partition(n - i, i, answer);
+            partition(n-i, i, answer + " " + i);
         }
-        //System.out.println("LOL: " + results);
     }
-
-	/**
-	 * clean printer method
-	 * @param result - the gathered result list from the provided input
-	 */
-	public static void printer() {
-		int length = 0;
-
-		length = results.size();
-
-		System.out.println("the results are:");
-
-		for (int i = 0; i < length; i++) {
-			System.out.print(i + ". ");
-
-			System.out.println(results.get(i));
-		}
-
-	}
 
 }
